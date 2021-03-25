3565758140b85824aafb50e52961e876233c01a7
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-25 12:53:53 +0300

    Был перенесены файлы обратно. Была добавлена картинка cafe в верстку приложения

--- app/src/main/java/com/example/money/InvoicesFragment.kt
+++ app/src/main/java/com/example/money/InvoicesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.money.fragment
+package com.example.money
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [OperationsFragment.newInstance] factory method to
+ * Use the [InvoicesFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class OperationsFragment : Fragment() {
+class InvoicesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class OperationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_operations, container, false)
+        return inflater.inflate(R.layout.fragment_invoices, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class OperationsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment OperationsFragment.
+         * @return A new instance of fragment InvoicesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            OperationsFragment().apply {
+            InvoicesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
