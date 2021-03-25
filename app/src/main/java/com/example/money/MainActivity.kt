9a1578437a40a79ba2d3080e15854e88fb7a3987
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-25 12:37:51 +0300

    Были добавлены фрагменты. В данном этопе происходит загрузка базовых картинок приложения

--- app/src/main/java/com/example/money/MainActivity.kt
+++ app/src/main/java/com/example/money/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.money
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -12,17 +14,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val navController = Navigation.findNavController(this,R.id.navigation)
         NavigationUI.setupWithNavController(bottomNavigationView,navController)
     }
-/*
-    <item
-    android:id="@+id/item_categories"
-    android:title="Категории"/>
-    <item
-    android:id="@+id/item_operations"
-    android:title="Операции"/>
-    <item
-    android:id="@+id/item_profile"
-    android:title="Профиль"/>*/
+
+    fun test (view: View)
+    {
+        Toast.makeText(this, "Клик", Toast.LENGTH_SHORT).show()
+    }
 }
