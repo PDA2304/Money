4c9d288c9600b57ae86b4cc7f5bdfa91373a53fe
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-30 11:25:22 +0300

    Была создана верстка для вкладки Профиль. Был добавлен файл для вида кнопки. Был изменен основной стиль. В файле activity_main.xml был изменен bottomnavigation

--- app/src/main/java/com/example/money/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/money/fragment/ProfileFragment.kt
@@ -5,7 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.money.R
+import com.github.florent37.shapeofview.shapes.ArcView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.appbar.*
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,12 +35,14 @@ class ProfileFragment : Fragment() {
         }
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        var view = inflater.inflate(R.layout.fragment_profile, container, false)
+        return view
     }
 
     companion object {
