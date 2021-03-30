327f9774fc03277801432cf2ee04c0e509bac23d
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-30 11:22:26 +0300

    При выборе вкладке Профиль меняется текст в appBare при выборе остальных вкладок appbar остается первоначальным.

--- app/src/main/java/com/example/money/MainActivity.kt
+++ app/src/main/java/com/example/money/MainActivity.kt
@@ -1,27 +1,52 @@
 package com.example.money
 
-import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.Toast
-import androidx.core.content.ContextCompat
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_invoices.*
+import kotlinx.android.synthetic.main.appbar.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navController = Navigation.findNavController(this,R.id.navigation)
-        NavigationUI.setupWithNavController(bottomNavigationView,navController)
+        val navController = Navigation.findNavController(this, R.id.navigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            navController.navigate(
+                item.itemId
+            )
+            var linear_prof = findViewById<LinearLayout>(R.id.linear_profile)
+            var linear_baze = findViewById<LinearLayout>(R.id.linear_baze)
+
+            when(item.itemId)
+            {
+                R.id.item_profile->
+                {
+                    linear_prof.visibility = View.VISIBLE
+                    linear_baze.visibility = View.GONE
+                    Log.i("ID_ID","TEST")
+                }
+                else ->
+                {
+                    linear_prof.visibility = View.GONE
+                    linear_baze.visibility = View.VISIBLE
+                }
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
+
     }
 
-    fun test (view: View)
-    {
+    fun test(view: View) {
         Toast.makeText(this, "Клик", Toast.LENGTH_SHORT).show()
     }
 }
