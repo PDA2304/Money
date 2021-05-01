4fc158d89f900acd7db884c970ef499165549952
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-05-01 08:01:23 +0300

    .

--- app/src/main/java/com/example/money/viewmodel/OnSaveDateViewModel.kt
+++ app/src/main/java/com/example/money/viewmodel/OnSaveDateViewModel.kt
@@ -2,7 +2,7 @@ package com.example.money.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.money.model.SaveDate
+//import com.example.money.model.SaveDate
 
 class OnSaveDateViewModel : ViewModel() {
     val savedate = MutableLiveData<String>()
